1c66c1ded57ca7466ed4b038e5c790f40b4d2cc3
Neo Yiu <neo.yiu@rakuten.com>
2017-08-16 21:22:50 +0100

    added codebase

---
+++ src/com/TestLeapYear/LeapYear.java
@@ -0,0 +1,13 @@
+package com.TestLeapYear;
+
+public class LeapYear {
+
+    public static boolean isLeap(final int year) {
+        return isDivisible(year, 4) && (!isDivisible(year, 100) || isDivisible(year, 400));
+    }
+
+    private static boolean isDivisible(int value, int denominator) {
+        return value % denominator == 0;
+    }
+
+}
